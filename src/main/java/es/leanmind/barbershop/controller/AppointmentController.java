9eabd14953c58e09b2dbfe6d996049081e14b812
Rosana Ruiz <rosanarufer@gmail.com>
2019-03-29 16:31:26 +0000

    Green: Appointment should be made by owner

--- src/main/java/es/leanmind/barbershop/controller/AppointmentController.java
+++ src/main/java/es/leanmind/barbershop/controller/AppointmentController.java
@@ -1,13 +1,21 @@
 package es.leanmind.barbershop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppointmentController {
 
     @GetMapping(Routes.appointments)
     public String show() {
+        return "appointments";
+    }
+
+    @PostMapping(Routes.appointments)
+    public String makeAppointment(Model model) {
+        model.addAttribute("responseMessage", "Guay!");
         return "appointments";
     }
 }
